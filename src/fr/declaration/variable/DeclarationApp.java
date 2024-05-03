e44e974b7665c17276a8392647b32916c93266c5
StevenD <dellarica.steven@gmail.com>
2024-05-03 15:47:37 +0200

    Création de DeclarationApp et ajout des variables.

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte byte1 = 1;
+        short short1 = 2;
+        int int1 = 3;
+        long long1 = 4;
+        float float1 = 5;
+        double double1 = 6;
+        char char1 = 'c';
+        boolean bool1 = true;
+        String string1 = "Coucou";
+
+        System.out.println(bool1);
+
+        String randomString = "Voici le résultat d'un calcul :  \n1+5=6";
+        System.out.println(randomString);
+    }
+}
